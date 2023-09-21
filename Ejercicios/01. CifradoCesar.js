cd2258441a1b4db42187e59db25be20f31955efc
Fernando Claro <fernandocdd02@gmail.com>
2023-09-21 22:46:20 +0200

    Ejemplos y ejercicio 1 incompleto

---
+++ Ejercicios/01. CifradoCesar.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    var abecedario = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+     'K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+    $(document).on('click', '#botonCifrar', function(){
+
+        var codigo = $('#codigo').val();
+
+        var posiciones = $('#numPosiciones').val();
+
+        var codigoCifrado = $('#resultado').val();
+
+        var codigoUpper = codigo.toUpperCase();
+
+        //alert(abecedario);
+        alert(codigoUpper);
+
+        
+
+    })
+});
