f61ce051343721b9306a8ffdbb425125fc7d9a35
Fernando <fernandocdd02@gmail.com>
2023-10-10 14:29:41 +0200

    Commit

---
+++ Ejemplo_OtroDTO/src/test/java/com/salesianostriana/dam/Ejemplo_OtroDTO/EjemploOtroDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.Ejemplo_OtroDTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploOtroDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Ejercicios/TypeScript/index.js
@@ -0,0 +1,24 @@
+/*var contador = 0;
+
+while (contador == 100){
+
+    contador++;
+}
+
+for (let index = 0; index < 100; index++) {
+    console.log(index);
+    
+}*/
+/*
+let mensaje = "Hola Mundo";
+var mensaje2 = "Hola Mundo 2";
+let a: number;
+let b: number;
+let suma: number;
+
+a = 10;
+b = 10;
+let suma = a + b;
+
+console.log(suma);
+*/ 
