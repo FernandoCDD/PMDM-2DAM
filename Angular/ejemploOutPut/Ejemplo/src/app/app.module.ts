81304e1ad0dce8a5f5bdee38286f128dcc7a5c66
Fernando <fernandocdd02@gmail.com>
2023-10-25 13:52:50 +0200

    Ej 7

--- Angular/ejemploOutPut/Ejemplo/src/app/app.module.ts
+++ Angular/ejemploOutPut/Ejemplo/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PersonajesListComponent } from './personajes-list/personajes-list.component';
-import { PersonajesItemComponent } from './personajes-item/personajes-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PersonajesListComponent,
-    PersonajesItemComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
