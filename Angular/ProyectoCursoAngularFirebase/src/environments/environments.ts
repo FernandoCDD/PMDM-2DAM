29bcdfb065da7ebd3b705733951d145e79d41dbb
Fernando <fernandocdd02@gmail.com>
2023-12-12 14:11:20 +0100

    Commit 2

---
+++ Angular/ProyectoCursoAngularFirebase/src/environments/environments.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: 'AIzaSyCkiy_w_qjMPIewLLn6oyJcTdcaIc_O_p8',
+        authDomain: 'proyectocursoangularfirebase.firebaseapp.com',
+        databaseURL: 'https://proyectocursoangularfirebase-default-rtdb.firebaseio.com',
+        projectId: 'proyectocursoangularfirebase',
+        storageBucket: 'proyectocursoangularfirebase.appspot.com',
+        messagingSenderId: '659735993899',
+        appId: '1:659735993899:web:5e0a0891a43d61576b546e',
+        measurementId: 'G-BHGC0F86P1'
+    }
+};
