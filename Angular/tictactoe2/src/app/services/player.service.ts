ee6fb3cbc753f719a15f556f51a56bafac68a9f1
Fernando <fernandocdd02@gmail.com>
2023-12-21 12:21:09 +0100

    Hecho el tablero y funciona, y hace el deploy

--- Angular/tictactoe2/src/app/services/player.service.ts
+++ Angular/tictactoe2/src/app/services/player.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
+import { addDoc, collection } from 'firebase/firestore';
 
+const COLLECTION_MATCHES = 'matches'
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
   constructor(public firestore: Firestore) { }
+
+  async createMatch(id: string){
+    const docRef = await addDoc(collection(this.firestore, COLLECTION_MATCHES),{
+      idPartida: id
+    });
+    console.log('ID: ', docRef.id);
+  }
 }
+
+/*
+async createUser(name: string, age: number) {
+    const docRef = await addDoc(collection(this.firestore, COLLECTION_USERS), {
+      name: name,
+      age: age,
+    });
+    console.log('Document written with ID: ', docRef.id);
+  }
+
+*/
