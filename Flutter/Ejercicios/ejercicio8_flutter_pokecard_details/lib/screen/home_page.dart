bc14bb385e7c82f94041cd7292143b104695efcc
Fernando <fernandocdd02@gmail.com>
2024-01-18 12:31:15 +0100

    Ejercicio de ListBuilder para casa

--- Flutter/Ejercicios/ejercicio8_flutter_pokecard_details/lib/screen/home_page.dart
+++ Flutter/Ejercicios/ejercicio8_flutter_pokecard_details/lib/screen/home_page.dart
@@ -1,11 +1,36 @@
 import 'package:ejercicio6_flutter_pokecard_api_get/widgets/pokemon_widget.dart';
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    HomePageScreen(
+      pokemons: List<PokemonWidget>.generate(10, (i) => 'Item $i'),
+    ),
+  );
+}
 class HomePageScreen extends StatelessWidget {
-  const HomePageScreen({super.key});
+  final List<PokemonWidget> pokemons;
+  const HomePageScreen({super.key, required this.pokemons});
 
   @override
   Widget build(BuildContext context) {
-    return const PokemonWidget();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Pokemones"),
+        ),
+      body: ListView.builder(
+        itemCount: .length,
+        prototypeItem: ListTile(
+          title: Text(items.first),
+        ),
+        itemBuilder: (context, items){
+          return ListTile(
+            
+          )
+        }
+      ),
+      ),
+    );
   }
 }
